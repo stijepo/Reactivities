416944b93912c03c56cb758f1dcf685959e47880
Stijepo Bronzic <sbronzic@snowfall.se>
2019-12-13 11:19:12 +0100

    Error Handling Middleware added

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -3,6 +3,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Persistence;
+using FluentValidation;
+using Application.Errors;
+using System.Net;
 
 namespace Application.Activities
 {
@@ -18,6 +21,23 @@ namespace Application.Activities
             public string City { get; set; }
             public string Venue { get; set; }
         }
+         public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Title).NotEmpty();
+
+                RuleFor(x => x.Description).NotEmpty();
+
+                RuleFor(x => x.Category).NotEmpty();
+                
+                RuleFor(x => x.Date).NotEmpty();
+                
+                RuleFor(x => x.City).NotEmpty();
+                
+                RuleFor(x => x.Venue).NotEmpty();
+            }
+        }
 
         public class Handler : IRequestHandler<Command>
         {
@@ -31,9 +51,9 @@ namespace Application.Activities
             {
                 var activity = await _context.Activities.FindAsync(request.Id);    
 
-                if (activity == null)
-                    throw new Exception("Could not find activity");
-
+                 if (activity == null)
+                    throw new RestException(HttpStatusCode.NotFound, new {Activity = "Not found"});
+                    
                 activity.Title = request.Title ?? activity.Title;            
                 activity.Description = request.Description ?? activity.Description;            
                 activity.Category = request.Category ?? activity.Category;            
